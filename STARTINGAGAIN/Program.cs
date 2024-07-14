2789076633d45b7db912bc03c071980bda69484b
manue <manue@LAPTOP-LTR24FQ7>
2024-07-14 19:44:54 -0400

    password checker

--- STARTINGAGAIN/Program.cs
+++ STARTINGAGAIN/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Globalization;
+using System.Threading;
 
-namespace  startingAgain 
+namespace startingAgain 
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
-           
+            Console.Write("Enter your new password: ");
+            string password = Console.ReadLine();
+
+
+            Console.Write("Confirm your password: ");
+            string passwordConfirm = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(password) && !string.IsNullOrEmpty(passwordConfirm))
+                Console.WriteLine(password.Equals(passwordConfirm) ? "Password match!" : "Password do not match!"); 
+            
+            else
+                Console.WriteLine("Please, enter a password.");
+         
         }
     }
 }
