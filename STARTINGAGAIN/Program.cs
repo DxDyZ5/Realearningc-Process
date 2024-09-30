805fba393c498edd34a3dbc7a32de0a7cd6e567f
manue <manue@LAPTOP-LTR24FQ7>
2024-09-30 18:37:23 -0400

    BUM! JAJAJAA

--- STARTINGAGAIN/Program.cs
+++ STARTINGAGAIN/Program.cs
@@ -1,11 +1,29 @@
-﻿namespace startingAgain
+﻿using System.Media;
+using System;
+
+namespace startingAgain
 {
     class Program
     {
 
         static void Main(string[] args)
         {
-            Console.WriteLine("nice");
+            if (OperatingSystem.IsWindows())
+            {
+                SoundPlayer player = new SoundPlayer("Shotgun Sound Effects.wav");
+                player.Load();
+                Console.WriteLine("SHOTGUN: ");
+                while (true)
+                {
+                    for (int i = 1; i > 0; i--)
+                    {
+                        Console.WriteLine($"AMMO:{i} BUM!  " ); player.Play();
+                        Console.WriteLine("Reloading...");
+                        Thread.Sleep(1000);
+                    }
+                }
+               
+            }
         }
 
     }
